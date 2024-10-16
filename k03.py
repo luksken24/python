7cf12f9526c477375b4ed299317f7d9bf1250ad7
luksken24 <ken@hkhk.edu.ee>
2024-10-16 11:31:58 +0300

    Create k03.py

---
+++ k03.py
@@ -0,0 +1,53 @@
+#16.10.24 Ken Luks
+#Ülesanne 03
+
+
+
+#ülesanne 3.2: ostunimekiri
+
+#ülesanne 3.4: lemmikraamat
+Raamatu_pealkiri = "Kaka ja kevad"
+Raamaru_autor = "Eno Raud"
+raamat = Raamatu_pealkiri+" "+Raamaru_autor
+lehekylgede_arv = 25
+hindamisskoor = 10
+print("Minu lemmikraamat on",Ra,"")
+
+
+
+
+
+
+#ülesanne 3.5 : Unistuste auto
+mark, mudel = "bmw", "118"
+auto = mark+" "+mudel
+aasta = 1997
+hind = 136.60
+print("Minu unistuste auto on",auto,"aasta",aasta,", mille hind on",hind,"eurot.")
+
+
+
+
+"""
+#Ülesanne 3.7: Python Turtle kolmnurk
+import turtle
+ 
+kylje_pikkus = 90
+nurk = 120
+kujundi_varv = "purple"
+
+turtle.color(kujundi_varv)
+
+for i in range(3):
+    for i in range(3):
+        turtle.forward(kylje_pikkus)
+        turtle.left(nurk)
+
+    turtle.penup()
+    turtle.forward(kylje_pikkus*1.5)
+    turtle.pendown()
+
+"""
+
+
+
