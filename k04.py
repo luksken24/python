fff93017450b86c9a9ecea1153239a18e9adecb5
luksken24 <ken@hkhk.edu.ee>
2024-10-16 11:45:51 +0300

    Create k04.py

---
+++ k04.py
@@ -0,0 +1,21 @@
+#16.10.24
+#ülesanne 04
+
+#2. raamatute allahindlus 
+ale = 0.3
+kogus = 3
+hind = 12.53
+summa = hind - (hind * ale) * kogus 
+print(f"{kogus} raamatu hind soodustusega on {summa:0.2f}€")
+
+
+
+
+#1.aia pikkus
+a = int(input("sisesta külg 1: "))
+b = int(input("sisesta külg 2: "))
+p = 2 * (a + b)
+print(f"Aia kogupikkus on {p} meetrit")
+
+
+
