0cc9ea83b376396c1fd2e480ad3f93248a24cfad
luksken24 <ken@hkhk.edu.ee>
2024-11-06 15:27:47 +0200

    Create KT.06.py

---
+++ KT.06.py
@@ -0,0 +1,54 @@
+import turtle
+
+
+for i in range(4):
+    turtle.lt(90)
+    for i in range(2):
+        turtle.fd(100)
+        turtle.left(90)
+        turtle.fd(50)
+        turtle.left(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()
