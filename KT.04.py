598117cfa02c38f0ddb20cb33bdd81f472d0a743
luksken24 <ken@hkhk.edu.ee>
2024-11-06 15:27:44 +0200

    Create KT.04.py

---
+++ KT.04.py
@@ -0,0 +1,22 @@
+#Kontrolltöö
+#06.11.24
+#Neljas
+import turtle 
+
+for i in range (5):
+    turtle.lt(360/5)
+    for i in range (5):
+        turtle.lt(360/5)
+        turtle.fd(50)
+        turtle.left(90)
+        turtle.fd(100)
+        turtle.left(90)
+        turtle.fd(100)
+        turtle.left(90)
+        turtle.fd(100)
+        turtle.left(90)
+        turtle.fd(50)
+
+
+
+turtle.done()
