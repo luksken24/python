b2ca9225708908631656665275ab8244cd6d3560
luksken24 <ken@hkhk.edu.ee>
2024-11-08 11:41:41 +0200

    Create K11.py

---
+++ K11.py
@@ -0,0 +1,33 @@
+# Ken Luks 8.11.24
+# Harjutus 11
+
+
+def pikim_sona(*sonad):
+    pikkim = 0
+    for i in sonad:
+        if len(i)>pikim:
+            pikim=len(i)
+        print(f"pikim sõna on {pikim} märki!")
+        
+
+
+pikim_sona("ükas", "kaks", "kolmkümmend")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
