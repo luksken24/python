6a383bb368ecff5234eefc278884d0e82ae71b83
luksken24 <ken@hkhk.edu.ee>
2024-12-18 15:36:57 +0200

    Update Kontrolltööd.py

---
+++ Kontrolltööd.py
@@ -0,0 +1,67 @@
+"""
+#10. Kaugushüpe
+for i in range (3):
+tulemused = 
+"""
+
+
+
+
+
+
+
+
+
+
+#1. Korrutamise kontrollimine
+
+
+import random
+
+for i in range (10):
+    arv1 = random.randint (1, 10)
+    arv2 = random.randint (1, 10)
+    õige_vastus = arv1 + arv2
+
+Kasutaja_vastus = int(input(f"{arv1} * {arv2} = "))
+
+if Kasutaja_vastus == õige_vastus:
+    print ("Õige!")
+else:
+    print (f"Vale. Õige vastus on {õige_vastus}")
+
+
+
+"""
+
+#11. Salakeel
+import base64
+try:
+    soov = input ("kas sa soovid sala keelt luua või tõlkida")
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+#16. Täringud
+
+
+
+
+
+
+
+
+
+
+
+
+#14. Palkade võrdlus
