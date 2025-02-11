e5834aed445e896ce16862569b2b7d8ede3fc64e
luksken24 <ken@hkhk.edu.ee>
2025-02-11 10:32:10 +0200

    Update KT.04.py

--- KT.04.py
+++ KT.04.py
@@ -3,18 +3,29 @@
 #Neljas
 import turtle 
 
-d = 100
+d = 70
 
 
 for i in range (5):
     turtle.lt(360/5)
+<<<<<<< HEAD
     turtle.fd(50)
     for i in range(3):
             turtle.left(90)
             turtle.fd(d)
             turtle.left(90)
             turtle.fd(50)
+=======
+    for i in range(4):
+            turtle.fd(d)
+            turtle.lt(90)
 
 
 
 turtle.done()
+
+
+>>>>>>> bafb209c85a77c351b994063299c7d35951799fe
+
+
+
