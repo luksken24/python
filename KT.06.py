59d621415293030594a99a956f1f1b7c918a0147
luksken24 <ken@hkhk.edu.ee>
2025-02-11 10:32:15 +0200

    Update KT.06.py

--- KT.06.py
+++ KT.06.py
@@ -1,53 +1,14 @@
 import turtle
 
+d = 100
 
 for i in range(4):
     turtle.lt(90)
     for i in range(2):
-        turtle.fd(100)
+        turtle.fd(d)
         turtle.left(90)
         turtle.fd(50)
         turtle.left(90)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
